b8acded532583a6f4513231a7543fab55cf05c2a
karmesin924 <jinhong0924@naver.com>
2026-03-05 17:06:18 +0900

    chore: initialize Vite React TypeScript project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        surface: {
+          900: '#0a0a0f',
+          800: '#12121a',
+          700: '#1a1a26',
+          600: '#22223a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
